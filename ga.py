b4b573827f0e76fa7314e44c0483758d8408f68f
GianCarloMilanese <giancarlo.milanese@gmail.com>
2019-07-02 23:01:18 +0200

    added modules for ga and sa

---
+++ ga.py
@@ -0,0 +1,372 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+
+def not_prime(n):
+    if n == 2:
+        return False
+    if n % 2 == 0 or n <= 1:
+        return True
+
+    sqr = int(math.sqrt(n)) + 1
+
+    for divisor in range(3, sqr, 2):
+        if n % divisor == 0:
+            return True
+    return False
+
+
+def is_prime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+
+
+def old_total_length_w_penalties(r, c, black_list, *args):
+    # v8
+    # every 10th city that is not prime is penalized,
+    # if it is inside the black list
+
+    # check if there are 0s at start and end
+    # r must be processed in order to have a 0 at the start
+    if r[0] != 0:
+        r = np.concatenate(([0], r))
+    if r[-1] == 0:
+        r = r[:-1]
+
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)
+    cid = cs[:, 0].astype(int)
+    d = np.sqrt((c[:, 1] - cs[:, 1]) ** 2 + (c[:, 2] - cs[:, 2]) ** 2)
+    #idx = np.arange(9, len(c), 10)
+    idx = np.arange(9, len(c), 10)
+    pc = cid[idx]
+    sel = (idx + 1)[black_list[pc]]
+    d[sel - 1] *= 1.1
+
+    return np.sum(d)
+
+
+def total_length_w_penalties(r, c, black_list, *args):
+    # CORRECT version
+    # every 10th city is penalized if it is inside the black list
+
+    # check if there are 0s at start and end
+    # r must be processed in order to have a 0 at the start
+    if r[0] != 0:
+        r = np.concatenate(([0], r))
+    if r[-1] == 0:
+        r = r[:-1]
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)
+    cid = c[:, 0].astype(int)
+    d = np.sqrt((c[:, 1] - cs[:, 1]) ** 2 + (c[:, 2] - cs[:, 2]) ** 2)
+    idx = np.arange(9, len(c), 10)
+    pc = cid[idx]
+    sel = (idx)[black_list[pc]]
+    d[sel] *= 1.1
+
+    return np.sum(d)
+
+
+def total_length_loop(r, c, *args):
+    # Just total length of a tour without penalization,
+    # starting and ending at the same city
+    # here 0 does not need to be at the start,
+    # since we do not need to keep track of non-prime 10th steps
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)
+    d = np.sqrt((c[:, 1] - cs[:, 1]) ** 2 + (c[:, 2] - cs[:, 2]) ** 2)
+
+    return np.sum(d)
+
+
+def total_length_straight(r, c, *args):
+    # Just total length of a tour without penalization,
+    # starting and ending at different cities
+    # here 0 does not need to be at the start,
+    # since we do not need to keep track of non-prime 10th steps
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)
+
+    # remove last row, corresponding to return to first city
+    cs = cs[:-1]
+    c = c[:-1]
+    d = np.sqrt((c[:, 1] - cs[:, 1]) ** 2 + (c[:, 2] - cs[:, 2]) ** 2)
+
+    return np.sum(d)
+
+
+def route_fitness(r, c, length_fun, **kwargs):
+    black_list = kwargs.get("black_list")
+    return 1 / length_fun(r, c, black_list)
+
+
+def subset_fitness(perm, c, subs, length_fun, **kwargs):
+    route = np.concatenate(subs[perm])
+    black_list = kwargs.get("black_list")
+    return 1 / length_fun(route, c)
+
+
+def pop_gen(n, pop_size, include_zero = True):
+    if include_zero:
+        s = 0
+    else:
+        s = 1
+    arr = np.repeat([np.arange(s, n)], pop_size, 0)
+    return np.apply_along_axis(np.random.permutation, 1, arr)
+
+
+def pop_eval(array, pop, fit_fun, **kwargs):
+    length_fun = kwargs.get("length_fun")
+    black_list = kwargs.get("black_list")
+    subs = kwargs.get("subs")
+    return np.apply_along_axis(fit_fun, 1, pop, c = array, length_fun = length_fun, black_list = black_list, subs = subs)
+
+
+def pop_stats(scores):
+    mean = np.mean(scores)
+    med = np.median(scores)
+    best = np.max(scores)
+    return med, mean, best
+
+
+def roulette_selection(pop, scores, size):
+    if size % 2 != 0:
+        size += 1
+    p = scores/np.sum(scores)
+    sel = np.random.choice(len(pop), size, replace = True, p = p).reshape((size//2, 2))
+    return np.concatenate((pop[sel][:,0], pop[sel][:,1]), 1)
+
+
+def roulette_selection2(pop, scores, size):
+    if size % 2 != 0:
+        size += 1
+    p = scores/np.sum(scores)
+    sel = np.random.choice(len(pop), size, replace = True, p = p).reshape((size//2, 2))
+    return pop[sel]
+
+
+def pairwise_crossover(p):
+    n = len(p) // 2
+    p0 = p[:n]
+
+    p1 = p[n:]
+    swath = np.random.randint(1, n)
+
+    cut1 = np.random.randint(0, n - swath)
+    cut2 = cut1 + swath
+
+    off = np.repeat([0], n)
+
+    sel = p0[cut1:cut2]
+    off[cut1:cut2] = sel
+    sel = p1[~np.isin(p1, sel)]
+    off[0:cut1] = sel[0:cut1]
+
+    off[cut2:] = sel[cut1:]
+    return off
+
+
+def two_point_crossover(pa):
+    return np.apply_along_axis(pairwise_crossover, 1, pa)
+
+
+def two_point_crossover2(p):
+    n = p.shape[2]
+    swath = np.random.randint(1, n)
+
+    cut1 = np.random.randint(0, n - swath)
+    cut2 = cut1 + swath
+
+    # print(cut1, cut2)
+
+    ps0 = p.shape[0]
+    off = np.zeros((ps0, n))
+
+    sel = p[:, 0, cut1:cut2]
+    off[:, cut1:cut2] = sel
+
+    p1 = p[:, 1]
+    sel = np.array(np.split(p1[~np.array([(np.isin(p1[i], sel[i])) for i in range(len(sel))])], ps0))
+
+    off[:, 0:cut1] = sel[:, 0:cut1]
+    off[:, cut2:] = sel[:, cut1:]
+    return off
+
+
+def mod_two_point_crossover2(p):
+    # http://www.rroij.com/open-access/enhanced-order-crossover-for-permutationproblems.php?aid=50178
+    n = p.shape[2]
+    swath = np.random.randint(1, n // 7)
+
+    cut1 = np.random.randint(0, n - swath)
+    cut2 = cut1 + swath
+
+    # print(cut1, cut2)
+
+    ps0 = p.shape[0]
+    off = np.zeros((ps0, n))
+
+    sel = p[:, 0, cut1:cut2]
+    off[:, cut1:cut2] = sel
+
+    p1 = p[:, 1]
+    sel = np.array(np.split(p1[~np.array([(np.isin(p1[i], sel[i])) for i in range(len(sel))])], ps0))
+
+    off[:, 0:cut1] = sel[:, 0:cut1]
+    off[:, cut2:] = sel[:, cut1:]
+    return off
+
+
+def two_point_crossover3(p):
+    n = p.shape[2]
+    swath = np.random.randint(1, n)
+    cut1 = np.random.randint(0, n - swath)
+    cut2 = cut1 + swath
+    ps0 = p.shape[0]
+
+    off = np.zeros((ps0, n))
+    sel = p[:, 0, cut1:cut2]
+    off[:, cut1:cut2] = sel
+    p1 = p[:, 1]
+
+    result = np.empty_like(p1)
+    for i in range(ps0):
+        result[i] = ~np.isin(p1[i], sel[i])
+    result = result.astype(bool)
+
+    sel = np.array(np.split(p1[result], ps0))
+
+    off[:, 0:cut1] = sel[:, 0:cut1]
+    off[:, cut2:] = sel[:, cut1:]
+    return off
+
+
+def swap_mutation(perm):
+    n = len(perm)
+    i = np.random.choice(n, 2, replace = False)
+    perm[i] = perm[i[::-1]]
+    return perm
+
+
+def reverse_mutation(perm):
+    n = len(perm) - 1
+    i = np.random.choice(n, 1)[0]
+    perm[i:i+2] = perm[i:i+2][::-1]
+    return(perm)
+
+
+def shift_mutation(perm):
+    n = len(perm)
+    i = np.random.choice(n, 2, replace = False)
+    i = np.sort(i)
+    i0 = i[0]
+    i1 = i[1]
+    perm = np.concatenate((perm[i1:], perm[i0:i1], perm[:i0][::-1]))
+    return perm
+
+
+def pop_mutation(pop, mut_fun, mut_perc):
+    sel = np.random.choice(len(pop), int(len(pop) * mut_perc), replace = False)
+    pop[sel] = np.apply_along_axis(mut_fun, 1, pop[sel])
+    return pop
+
+
+def GA(array, n_gen, pop_size, parent_size, fit_fun, mut_funs, mut_perc=0.1, sel_fun=roulette_selection2,
+       cross_fun=None, pop_include_zero=True, max_no_change=100, on_subsets=False, verbose=False, **kwargs):
+    subs = kwargs.get("subs")
+    if on_subsets:
+        n = len(subs)
+    else:
+        n = len(array)
+
+    # init pop
+    pop = pop_gen(n, pop_size, pop_include_zero)
+
+    length_fun = kwargs.get("length_fun")
+    black_list = kwargs.get("black_list")
+
+    scores = pop_eval(array, pop, fit_fun, length_fun=length_fun, black_list=black_list, subs=subs)
+    med, mean, best = pop_stats(scores)
+
+    ov_best = best  # overall best score init
+    best_pop = pop  # overall best pop init
+    med_trace = np.array([med])  # trace of median scores init
+    mean_trace = np.array([mean])  # trace of mean scores init
+    best_trace = np.array([best])  # trace of best scores init
+
+    iter_no_change = 0
+    i = 0
+
+    while i < n_gen:
+        # pass n_gen = np.inf to iterate until iter_no_change >= max_no_change
+
+        # select best parents
+        if verbose: start = datetime.now()
+        parents = sel_fun(pop, scores, parent_size)
+        if verbose:
+            end = datetime.now()
+            sel_sec = (end - start).total_seconds()
+
+        if verbose: start = datetime.now()
+        if not (cross_fun is None):
+            # generate offsprings
+            offs = cross_fun(parents)
+        if verbose:
+            end = datetime.now()
+            gen_sec = (end - start).total_seconds()
+
+        # replace worst in pop with offs
+        if not (cross_fun is None):
+            sort = np.argsort(scores)
+            pop[sort[:len(offs)]] = offs
+
+        # mutate pop
+        if verbose: start = datetime.now()
+        for fun in mut_funs:
+            pop = pop_mutation(pop, fun, mut_perc)
+        if verbose:
+            end = datetime.now()
+            mut_sec = (end - start).total_seconds()
+
+        # evaluate
+        if verbose: start = datetime.now()
+        # scores = pop_eval(array, pop, fit_fun)
+        scores = pop_eval(array, pop, fit_fun, length_fun=length_fun, black_list=black_list, subs=subs)
+        if verbose:
+            end = datetime.now()
+            ev_sec = (end - start).total_seconds()
+
+        # update traces
+        med, mean, best = pop_stats(scores)  # new med, mean best scores
+
+        if best > ov_best:
+            ov_best = best
+            best_pop = pop.copy()
+            iter_no_change = 0
+
+        med_trace = np.concatenate((med_trace, [med]))
+        mean_trace = np.concatenate((mean_trace, [mean]))
+        best_trace = np.concatenate((best_trace, [best]))
+
+        if verbose:
+            if i % 100 == 0:
+                text = [i, iter_no_change, sel_sec, gen_sec, mut_sec, ev_sec]
+                text.append(sum(text[2:]))
+                text.append(1 / ov_best)
+                print('Iter {}, ItNoChange {}, Sel {}, Cross {}, Mut {}, Ev {}, Tot {}, Best {}'.format(*text))
+
+        i += 1
+        iter_no_change += 1
+        if iter_no_change >= max_no_change:
+            break
+
+    # best_scores = pop_eval(array, best_pop, fit_fun)
+    best_scores = pop_eval(array, best_pop, fit_fun, length_fun=length_fun, black_list=black_list, subs=subs)
+    bpe = best_pop[np.argmax(best_scores)]
+
+    return med_trace, mean_trace, best_trace, ov_best, bpe
---
+++ sa.py
@@ -0,0 +1,212 @@
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import numpy as np
+
+def total_length_w_penalties(r, c, black_list, *args):
+    # CORRECT version
+    # every 10th city is penalized if it is inside the black list
+
+    # check if there are 0s at start and end
+    # r must be processed in order to have a 0 at the start
+    if r[0] != 0:
+        r = np.concatenate(([0], r))
+    if r[-1] == 0:
+        r = r[:-1]
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)
+    cid = c[:, 0].astype(int)
+    d = np.sqrt((c[:, 1] - cs[:, 1]) ** 2 + (c[:, 2] - cs[:, 2]) ** 2)
+    idx = np.arange(9, len(c), 10)
+    pc = cid[idx]
+    sel = (idx)[black_list[pc]]
+    d[sel] *= 1.1
+
+    return np.sum(d)
+
+
+def total_length_w_penalties_unordered(r, c, black_list):
+    n_id = np.arange(len(c))[:, np.newaxis]  # new_index
+    c = np.concatenate((n_id, c), 1)
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)[:-1]
+    c = c[:-1]
+    cid = c[:, 1].astype(int)
+    d = np.sqrt((c[:, 2] - cs[:, 2]) ** 2 + (c[:, 3] - cs[:, 3]) ** 2)
+    idx = np.arange(8, len(c), 10)
+    pc = cid[idx]
+    sel = (idx)[black_list[pc]]
+    d[sel] *= 1.1
+
+    return np.sum(d)
+
+
+def total_length_w_penalties_unordered_straigth(r, c, black_list):
+    n_id = np.arange(len(c))[:, np.newaxis]  # new_index
+    c = np.concatenate((n_id, c), 1)
+    c = c[r, :]
+    cs = np.roll(c, -1, axis=0)[:-1]
+    c = c[:-1]
+    cid = c[:, 1].astype(int)
+    d = np.sqrt((c[:, 2] - cs[:, 2]) ** 2 + (c[:, 3] - cs[:, 3]) ** 2)
+    idx = np.arange(8, len(c), 10)
+    pc = cid[idx]
+    sel = (idx)[black_list[pc]]
+    d[sel] *= 1.1
+
+    return np.sum(d)
+
+
+def shift_mutation(perm):
+    n = len(perm)
+    i = np.random.choice(n, 2, replace = False)
+    i = np.sort(i)
+    i0 = i[0]
+    i1 = i[1]
+    perm = np.concatenate((perm[i1:], perm[i0:i1], perm[:i0][::-1]))
+    return perm
+
+
+def swap_mutation(perm, *args):
+    n = len(perm)
+    i = np.random.choice(n, 2, replace = False)
+    perm[i] = perm[i[::-1]]
+    return perm
+
+
+def reverse_mutation(perm, *args):
+    n = len(perm) - 1
+    i = np.random.choice(n, 1)[0]
+    perm[i:i+2] = perm[i:i+2][::-1]
+    return(perm)
+
+
+def shift_primes_mutation(sub, perm, black_list, scale, n):
+    l = len(perm)
+    cids = sub[:, 0].astype(int)
+    bool_mask = black_list[cids[perm]]
+    tenths = np.arange(8, l, 10)  # 8, 9, 10??
+    p1 = bool_mask.astype(int)
+    p1[tenths] *= scale
+    p1[p1 == 0] = 1
+    p1 = (p1 / sum(p1))
+    idx1 = np.random.choice(np.arange(l), n, replace=False, p=p1)
+
+    p2 = (~bool_mask).astype(int)
+
+    p2[np.delete(np.arange(l), tenths)] *= scale
+    p2[p2 == 0] = 1
+
+    p2[idx1] = 0
+
+    p2 = (p2 / sum(p2))
+    idx2 = np.random.choice(np.arange(l), n, replace=False, p=p2)
+
+    newperm = perm.copy()
+    newperm[idx1] = perm[idx2]
+    newperm[idx2] = perm[idx1]
+
+    return newperm
+
+
+def reverse_primes_mutation(sub, perm, black_list, scale, n):
+    l = len(perm)
+    cids = sub[:, 0].astype(int)
+    bool_mask1 = black_list[cids[perm]]
+    bool_mask2 = ~np.roll(bool_mask1, -1)
+    bool_mask = bool_mask1 & bool_mask2
+
+    tenths = np.arange(8, l, 10)
+
+    p1 = bool_mask.astype(int)
+    p1[tenths] *= scale
+    p1[p1 == 0] = 1
+    p1[np.arange(1, l, 2)] = 0
+
+    p1 = p1[:-1]
+    p1 = (p1 / sum(p1))
+    i = np.random.choice(np.arange(l - 1), n, replace=False, p=p1)
+
+    newperm = perm.copy()
+    newperm[i] = perm[i + 1]
+    newperm[i + 1] = perm[i]
+
+    return newperm
+
+
+def SA(array, fit_fun, maxIter, mut_fun, black_list, scale, n_to_mute,
+       maxIterNoChange=200, tmin=0.01, alpha=0.999, perm_init=None, t_init=1000, verbose=False):
+    # initialize solution
+    if perm_init is None:
+        perm = np.random.permutation(range(1, len(array)))
+    else:
+        perm = perm_init.copy()
+
+    n = len(perm)
+
+    # init temperature
+    tem = t_init
+
+    dist = fit_fun(perm, array, black_list)  # objective function
+    best_dist = dist
+    best_trace = np.array([dist])
+    current_trace = np.array([dist])
+    best_perm = perm.copy()
+
+    citer = 0
+    iterNoChange = 0
+
+    while tem >= tmin:
+
+        # mutate
+        newperm = mut_fun(array, perm, black_list, scale, n_to_mute)
+
+        dist_new = fit_fun(newperm, array, black_list)
+        # print(newperm)
+        # print(dist_new)
+
+        if dist_new <= best_dist:
+            # print(dist_new)
+            # print('New is best')
+            perm = newperm
+            dist = dist_new
+            best_dist = dist
+            best_perm = perm.copy()
+            iterNoChange = 0
+        elif (np.exp((dist - dist_new) / tem) > np.random.uniform()):
+
+            # print('New is worse')
+            dist = dist_new
+            perm = newperm
+            iterNoChange = 0
+
+        # print('Current dist: ', dist, 'Best dist: ', best_dist)
+        best_trace = np.concatenate((best_trace, [best_dist]))
+        current_trace = np.concatenate((current_trace, [dist]))
+        tem *= alpha
+
+        if (citer % 100 == 0) and verbose:
+            print('Iter: {}, IterNoChange: {}, Current: {}, Best: {}'.format(citer, iterNoChange, dist, best_dist))
+
+        citer += 1
+        iterNoChange += 1
+        if (iterNoChange >= maxIterNoChange) or (citer >= maxIter):
+            break
+
+        # res = list(route=path, traceBest = traceBest, trace = traceCurrentLength)
+    return best_perm, best_trace, current_trace
+
+
+def plot_SA(b, c, n):
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    x = np.arange(0, len(c), n)
+    l2 = ax.plot(x, b[::n])[0]
+    l3 = ax.plot(x, c[::n])[0]
+
+    line_labels = ["Best", "Current"]
+
+    fig.legend([l2, l3], line_labels, bbox_to_anchor=(0.85, 0.25))
+
+    plt.show()
